8f56053b35d420be5887daf45847652a7d002838
Juan Andrés Miranda <tu_correo@ejemplo.com>
2026-09-20 21:34:46 -0300

    Entrega Semana 6

---
+++ PRY2204_Exp3_S6_S.sql
@@ -0,0 +1,130 @@
+Procedimiento PL/SQL terminado correctamente.
+
+Table ESPECIALIDAD creado.
+
+
+Table COMUNA creado.
+
+
+Table DIAGNOSTICO creado.
+
+
+Table BANCO creado.
+
+
+Table DIGITADOR creado.
+
+
+Table MEDICO creado.
+
+
+Table PACIENTE creado.
+
+
+Table RECETA creado.
+
+
+Table MEDICAMENTO creado.
+
+
+Table DOSIS creado.
+
+
+Table PAGO creado.
+
+
+Table ESPECIALIDAD alterado.
+
+
+Table COMUNA alterado.
+
+
+Table DIAGNOSTICO alterado.
+
+
+Table BANCO alterado.
+
+
+Table DIGITADOR alterado.
+
+
+Table MEDICO alterado.
+
+
+Table PACIENTE alterado.
+
+
+Table RECETA alterado.
+
+
+Table MEDICAMENTO alterado.
+
+
+Table DOSIS alterado.
+
+
+Table PAGO alterado.
+
+
+Table MEDICO alterado.
+
+
+Table PACIENTE alterado.
+
+
+Table RECETA alterado.
+
+
+Table RECETA alterado.
+
+
+Table RECETA alterado.
+
+
+Table RECETA alterado.
+
+
+Table DOSIS alterado.
+
+
+Table DOSIS alterado.
+
+
+Table PAGO alterado.
+
+
+Table PAGO alterado.
+
+
+Table MEDICO alterado.
+
+
+Table PACIENTE alterado.
+
+
+Table MEDICO alterado.
+
+
+Table DIGITADOR alterado.
+
+
+Table RECETA alterado.
+
+
+Table MEDICAMENTO alterado.
+
+
+Table MEDICAMENTO alterado.
+
+
+Table MEDICAMENTO alterado.
+
+
+Table PAGO alterado.
+
+
+Table PACIENTE alterado.
+
+
+Table PACIENTE alterado.
+
